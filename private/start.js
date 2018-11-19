e02048dc14f79b44d0aaaf11f28293cef327e398
Gianmarco <gmnco96@gmail.com>
2018-11-19 16:12:06 +0100

    a

--- private/start.js
+++ private/start.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+
+app.get('/', function(req, res, next) {
+    console.log("Pagina ricevuta!");
+    next();
 });
 
+
+app.use('/', express.static('public'));
+
 app.listen(8080, function () {
-    console.log('Server avviato. In ascolto sulla porta 8080!');
+    console.log('SoundWave - Server avviato. In ascolto sulla porta 8080!');
 });
 
 
