1b316d632acd8b9d47b7d84452915445800573e4
giuseppe <peppe.moscarelli96@gmail.com>
2018-11-25 19:44:01 +0100

    Merge branch 'master' of https://github.com/Sbugheaz/Progetto
    # Conflicts:
    #	.vagrant/machines/default/virtualbox/action_provision
    #	.vagrant/machines/default/virtualbox/action_set_name
    #	.vagrant/machines/default/virtualbox/id
    #	.vagrant/machines/default/virtualbox/index_uuid
    #	.vagrant/machines/default/virtualbox/private_key
    #	public/stylesheets/webPlayer.css
    #	public/webPlayer.html
    #	ubuntu-xenial-16.04-cloudimg-console.log

---
+++ public/javascripts/webPlayer.js
@@ -0,0 +1,15 @@
+
+//funzione che mostra le password nascoste
+function mostraPass(id){
+    var x = document.getElementById(id);
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+//funzione che rende scrivibili i campi dei form
+function abilitaScrittura(id){
+    document.getElementById(id).readOnly = false;
+}
