f752b5ec9b95abbdd879d76754de33b52926c192
Gianmarco <gmnco96@gmail.com>
2018-11-26 17:49:34 +0100

    a

--- public/javascripts/webPlayer.js
+++ public/javascripts/webPlayer.js
@@ -1,11 +1,14 @@
 
 //funzione che mostra le password nascoste
-function mostraPass(id){
+function mostraPass(id, id2){
     var x = document.getElementById(id);
+    var y = document.getElementById(id2);
     if (x.type === "password") {
         x.type = "text";
+        y.className = "fa fa-eye-slash iconaPassword";
     } else {
         x.type = "password";
+        y.className = "fa fa-eye iconaPassword";
     }
 }
 
