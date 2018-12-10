9bb081446575721c51afedbc29e448befc79f574
giuseppe <peppe.moscarelli96@gmail.com>
2018-12-10 09:56:21 +0100

    Merge branch 'master' of https://github.com/Sbugheaz/Progetto
    # Conflicts:
    #	.vagrant/machines/default/virtualbox/action_provision
    #	.vagrant/machines/default/virtualbox/action_set_name
    #	.vagrant/machines/default/virtualbox/id
    #	.vagrant/machines/default/virtualbox/index_uuid
    #	.vagrant/machines/default/virtualbox/private_key
    #	public/stylesheets/webPlayer.css
    #	public/webPlayer.html
    #	ubuntu-xenial-16.04-cloudimg-console.log

--- public/javascripts/webPlayer.js
+++ public/javascripts/webPlayer.js
@@ -15,15 +15,33 @@ function mostraPass(id, id2){
     }
 }
 
-//funzione che rende scrivibili i campi dei form
+//funzione che rende modificabili i campi dei form
 function abilitaScrittura(id){
     document.getElementById(id).readOnly = false;
 }
 
+//funzione che mostra il footer del modal profilo quando un campo viene modificato
 $(document).ready(function(){
-$(".pulsanteModifica").click(function(){
-    $(".modal-footer").show();
+$(".campi").on('input',function(){
+    $(".modal-footer").show("display");
 });
+});
+
+
+//funzione che gestisce la sovrapposizione dei modal
+$(document).ready(function () {
+    $('#openBtn').click(function () {
+        $('#myModal').modal({
+            show: true
+        })
+    });
+    $(document).on('show.bs.modal', '.modal', function () {
+        var zIndex = 1040 + (10 * $('.modal:visible').length);
+        $(this).css('z-index', zIndex);
+        setTimeout(function() {
+            $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+        }, 0);
+    });
 });
 
 
@@ -49,3 +67,10 @@ $(document).ready(function(){
     });
 });
 
+$(document).ready(function(){
+    $("#pulsante-aggiungiAmici").click(function(){
+        $("#pulsante-aggiungiAmici").css("width", "auto").slideUp(2000).slideDown(2000);
+
+
+    });
+});
