8d1d81be538c7cf621c426086ca0574bba1ff7e9
giuseppe <peppe.moscarelli96@gmail.com>
2019-02-11 18:14:35 +0100

    Merge branch 'master' of https://github.com/Sbugheaz/Progetto
    # Conflicts:
    #	www/public/javascripts/webPlayer.js

--- www/public/javascripts/start.js
+++ www/public/javascripts/start.js
@@ -1,6 +1,10 @@
+//dichiarazione degli oggetti
 var utente;
 
+//funzione eseguita al caricamento della pagina
 $(document).ready(function () {
+
+    //funzione che inizializza i dati dell'account estrapolandoli dall'oggetto JSON ricevuto dal server
     $.get('/WebPlayer/utente', function(result){
         utente = new Account(JSON.parse(result));
         $(".nomeUtente").html("<br>" + utente.nomeUtente);
@@ -9,4 +13,16 @@ $(document).ready(function () {
         $('#dataNascita').attr("value", utente.dataDiNascita.substring(0,10));
         $('#email').attr("value",utente.email);
     });
+
+
+    $.get('/WebPlayer/amici', function(result){
+        utente = new Account(JSON.parse(result));
+        $(".nomeUtente").html("<br>" + utente.nomeUtente);
+        $('#nome').attr("value",utente.nome);
+        $('#cognome').attr("value",utente.cognome);
+        $('#dataNascita').attr("value", utente.dataDiNascita.substring(0,10));
+        $('#email').attr("value",utente.email);
+    });
+
 });
+
