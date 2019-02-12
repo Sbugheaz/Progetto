6bdab19cb810f86c0a48817062770a96d4f70b74
giuseppe <peppe.moscarelli96@gmail.com>
2019-02-12 11:14:16 +0100

    Merge branch 'master' of https://github.com/Sbugheaz/Progetto
    # Conflicts:
    #	www/public/javascripts/webPlayer.js

--- www/public/javascripts/start.js
+++ www/public/javascripts/start.js
@@ -4,7 +4,7 @@ var utente, listaAmici = [];
 //funzione eseguita al caricamento della pagina
 $(document).ready(function () {
 
-    //funzione che inizializza i dati dell'account estrapolandoli dall'oggetto JSON ricevuto dal server
+    //funzione che inizializza i dati dell'account estrapolandoli dall'oggetto JSON ricevuto dal server e li stampa nel form
     $.get('/WebPlayer/utente', function(result){
         utente = new Account(JSON.parse(result)[0]);
         $(".nomeUtente").html("<br>" + utente.nomeUtente);
@@ -14,11 +14,32 @@ $(document).ready(function () {
         $('#email').attr("value",utente.email);
     });
 
+    //funzione che riceve dal database i dati relativi agli amici di un utente e li stampa nell'apposita lista
     $.get('/WebPlayer/amici', function(result){
         var la = JSON.parse(result);
+        var content="";
+        $(".contenitore-lista-amici").append('<ul class="demo listaAmici" style="color:cornsilk;">');
         for(i=0; i<la.length; i++) {
             listaAmici[i] = new Account(la[i]);
+            content += '<li class="amico">' +
+                '<div class="datiAmico nomeAmico">' + listaAmici[i].nome + '</div>' +
+                '<div class="datiAmico cognomeAmico">' + listaAmici[i].cognome + '</div>' +
+                '<div class="datiAmico nomeUtenteAmico">' + listaAmici[i].nomeUtente + '</div>' +
+                '<div class="datiAmico container-icona-rimuovi-amico">' +
+                '<i class="fa fa-user-times icona-rimuovi-amico" id="rimuovi-amico' + listaAmici[i].idUtente +'"></i> </div>' +
+                '</li>' ;
+            $(".listaAmici").append(content);
+            content = "";
+
         }
+
+
+
+        /*
+        $(".nomeAmico").html(listaAmici[0].nome);
+        $(".cognomeAmico").html(listaAmici[0].cognome);
+        $(".nomeUtenteAmico").html(listaAmici[0].nomeUtente);
+        */
     });
 
 });
